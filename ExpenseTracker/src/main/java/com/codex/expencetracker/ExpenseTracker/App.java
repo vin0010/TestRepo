d43a23e9e1b99edf8f928bdf0d475c622b10400f
Vinoth G <vin10catchme@gmail.com>
2018-11-30 16:14:55 +0530

    Expense Tracker project initial commit

---
+++ ExpenseTracker/src/main/java/com/codex/expencetracker/ExpenseTracker/App.java
@@ -0,0 +1,13 @@
+package com.codex.expencetracker.ExpenseTracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
